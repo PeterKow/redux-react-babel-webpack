7a8bb7878e9ae98a337c69e7cb92795b6c231dd2
PeterKow <czerwonymm@gmail.com>
2015-09-30 13:32:20 -0400

    login router check

---
+++ src/app/pages/loginPage/loginPage.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Peter on 30/09/15.
+ */
+import React, { Component } from 'react'
+
+export default class Login extends Component {
+
+  render() {
+    return <h1>Login</h1>
+  }
+
+}
--- src/app/routes.js
+++ src/app/routes.js
@@ -6,14 +6,22 @@ import Application from './pages/mainPage/app.js'
 import MainPage from './pages/mainPage/mainPage.js'
 import NotFoundRouteView from './pages/utils/notFoundRoute.js'
 import SearchArticles from './pages/mainSearchPage/searchArticles.js'
+import Login from './pages/loginPage/loginPage.js'
+
+function requireAuth(nextState, replaceState) {
+  //if (!auth.loggedIn())
+  if (true)
+    replaceState({ nextPathname: nextState.location.pathname }, '/login')
+}
 
 function renderRoutes (history) {
   return (
     <Router history={history}>
       <Route component={ Application }>
-        <Route path="/" component={ MainPage } />
+        <Route path="/" component={ MainPage } onEnter={requireAuth}/>
       </Route>
 
+      <Route path="login" component={ Login } />
       <Route path="search" component={ SearchArticles } />
       <Route path="*" component={ NotFoundRouteView }/>
     </Router>
