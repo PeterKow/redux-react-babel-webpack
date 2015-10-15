a85129b540f6b044427726f6b130161a867292b7
PeterKow <czerwonymm@gmail.com>
2015-10-15 20:16:34 +0100

    test for component miniArticle - Render Test

--- src/test/app/containers/articles/components/miniArticle.spec.js
+++ src/test/app/containers/articles/components/miniArticle.spec.js
@@ -1,5 +1,29 @@
 import { expect } from 'chai'
-import jsdom from 'jsdom'
+import React from 'react'
 import ReactTestUtils from 'react-addons-test-utils';
+import { twitterResultsSimple } from '../../../../../app/containers/articles/mockTwitterResults.js'
 
-import miniArticle from '../../../../../app/containers/articles/components/miniArticle.jsx'
+import MiniArticle from '../../../../../app/containers/articles/components/miniArticle.jsx'
+
+function setup(){
+
+  const renderer = ReactTestUtils.createRenderer()
+  renderer.render(
+    <MiniArticle {...twitterResultsSimple[0]}
+      key={twitterResultsSimple[0].id}
+      onClick={() => { console.log('index', twitterResultsSimple[0].id)}} /> )
+
+   const renderOutput = renderer.getRenderOutput()
+
+  return { renderer, renderOutput}
+}
+
+describe('MiniArticle component', function() {
+
+  it('should render correctly', function(){
+    const output = setup()
+    expect(output.renderOutput.props.children.props.style.padding).to.be.equal('9px 12px');
+    expect(output.renderOutput.props.children.props.children.props.children[1]).to.be.equal(twitterResultsSimple[0].text);
+  })
+
+})
