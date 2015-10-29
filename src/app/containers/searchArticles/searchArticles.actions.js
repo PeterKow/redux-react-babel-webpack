550c429851ac22af9e8c17e3dd169c5b38a3dfaf
PeterKow <czerwonymm@gmail.com>
2015-10-29 14:19:06 +0000

    already in refactor

--- src/app/containers/searchArticles/searchArticles.actions.js
+++ src/app/containers/searchArticles/searchArticles.actions.js
@@ -1,12 +1,6 @@
 import { SEARCH_ARTICLES } from './searchArticles.actionTypes.js'
 
 export function searchArticles(newSearch) {
-  return dispatch => {
-    fetch('/auth/twitter')
 
-      .then(res => dispatch(newMiniArticles(randomResponse())))
-      .catch(res => console.log('ended BADD!!! from fetchMiniArticles', res));
-
-  }
   return { type: SEARCH_ARTICLES, newSearch};
 }
